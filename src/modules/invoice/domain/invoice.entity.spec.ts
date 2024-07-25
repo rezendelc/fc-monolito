82153bcd6dd56274b4ffe3f7c9e71beacb5b4918
rezendelc <rezendelc94@gmail.com>
2024-07-25 09:59:26 -0300

    fix docker and add test to invoice entity

---
+++ src/modules/invoice/domain/invoice.entity.spec.ts
@@ -0,0 +1,33 @@
+import Address from '../../@shared/domain/value-object/address'
+import InvoiceItem from './invoice-item.entity'
+import Invoice from './invoice.entity'
+
+describe("Methods of invoice entity unit test", () => {
+  it("should sum all invoice items price", () => {
+    const invoiceItem1 = new InvoiceItem({
+      name: "Item1",
+      price: 7
+    })
+
+    const invoiceItem2 = new InvoiceItem({
+      name: "Item2",
+      price: 9
+    })
+
+    const invoice = new Invoice({
+      name: "invoice",
+      document: "document",
+      address: new Address(
+        "street",
+        "number",
+        "complement",
+        "city",
+        "state",
+        "zipCode",
+      ),
+      items: [invoiceItem1, invoiceItem2]
+    })
+
+    expect(invoice.total).toBe(16)
+  })
+})
